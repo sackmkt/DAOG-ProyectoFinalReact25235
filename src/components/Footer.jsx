9961a14caea85aaa98e1a6ee698b0f83e8ddc035
sackmkt <diego95ojeda@gmail.com>
2025-10-22 23:18:17 -0300

    Continuando con la pre-entrega

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import { Container } from "react-bootstrap";
+
+
+const Footer = () => {
+return (
+
+    <footer className="bg-dark text-white text-center py-4 mt-5">
+        <Container>
+            <p className="mb-0"> Pre-Entrega 2025 - Todos los derechos reservados: 1.12.0.1.</p>
+        </Container>
+
+    </footer>
+
+);
+}
+
+export default Footer;
